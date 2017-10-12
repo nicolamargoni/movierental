12cc9f07727c0c6f121ee542e5816d8f3e81ff87
nicolamargoni <nicola.margoni@xpeppers.com>
2017-10-12 17:35:39 +0200

    refactor: Replace Conditional with Polymorphism, getCharge()

--- java/src/main/java/NewReleasePrice.java
+++ java/src/main/java/NewReleasePrice.java
@@ -2,4 +2,9 @@ public class NewReleasePrice extends Price{
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    double getCharge(int daysRented){
+        double result = daysRented * 3;
+        return result;
+    }
 }
