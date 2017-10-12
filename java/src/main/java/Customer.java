1429c7d2861c3e42b820fabc4510be60e97d797a
nicolamargoni <nicola.margoni@xpeppers.com>
2017-10-12 16:19:59 +0200

    refactor: Extract Method and Replace Temp with Query, totalAmount -> getTotalCharge()

--- java/src/main/java/Customer.java
+++ java/src/main/java/Customer.java
@@ -30,14 +30,23 @@ public class Customer {
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
 
         }
 
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
+        return result;
+    }
+
+    public double getTotalCharge(){
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()){
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 
