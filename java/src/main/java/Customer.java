dd1a1f6c1c7fcd517a0d399862ccc1ba2fbca2aa
nicolamargoni <nicola.margoni@xpeppers.com>
2017-10-12 15:33:18 +0200

    refactor: Move Method, amountFor from Customer.class to Rental.class

--- java/src/main/java/Customer.java
+++ java/src/main/java/Customer.java
@@ -51,24 +51,10 @@ public class Customer {
     }
 
     private double amountFor(Rental aRental){
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRendeted() > 2)
-                    result += (aRental.getDaysRendeted() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRendeted() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRendeted() > 3)
-                    result += (aRental.getDaysRendeted() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return aRental.amountFor();
     }
+
+
 
 
 }
--- java/src/main/java/Rental.java
+++ java/src/main/java/Rental.java
@@ -15,4 +15,24 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    public double amountFor(){
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRendeted() > 2)
+                    result += (getDaysRendeted() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRendeted() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRendeted() > 3)
+                    result += (getDaysRendeted() - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
