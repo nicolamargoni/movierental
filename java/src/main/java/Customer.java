3d2c4561ef0383fd0e44d46a37c673c8baf4d48b
nicolamargoni <nicola.margoni@xpeppers.com>
2017-10-12 15:56:46 +0200

    refactor: Replace Temp with Query, thisAmount -> each.getCharge()

--- java/src/main/java/Customer.java
+++ java/src/main/java/Customer.java
@@ -27,8 +27,6 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints++;
 
@@ -38,8 +36,8 @@ public class Customer {
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
 
         }
 
